5003f6cd431e5eefc917d7dce54d6f22075ec9b4
Mauricio Velazquez <mauriciovelazquez211@gmail.com>
2025-04-16 22:46:59 -0300

    Bug arreglado

--- backend/src/main/java/ar/edu/unq/spring/controller/utils/Validator.java
+++ backend/src/main/java/ar/edu/unq/spring/controller/utils/Validator.java
@@ -11,7 +11,7 @@ public class Validator {
     private static final String CONTRASENIA_CANT_CARACTERES_MIN = "La contraseña debe tener un mínimo de 6 caracteres.";
     private static final String CONTRASENIA_NO_ALFANUMERICA = "La contraseña debe ser alfanumérica.";
     private static final String DNI_VACIO_O_NULO = "El DNI no puede estar vacio o nulo.";
-    private static final String DNI_CANT_CARACTERES_MIN = "El DNI debe tener un mínimo de 8 caracteres.";
+    private static final String DNI_CANT_NUMEROS = "El DNI debe tener solo 8 números.";
     private static final String DNI_SOLO_ADMITE_NUMEROS = "El DNI debe contener solo números.";
     private static final String PATRON_CONTRASENIA_ALFANUMERICA = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]+$";
     private static final String PATRON_DNI_NUMERICO = "^\\d+$";
@@ -40,7 +40,10 @@ public class Validator {
             throw new IllegalArgumentException(DNI_SOLO_ADMITE_NUMEROS);
         }
         if (createPacienteDTO.dni().length() < 8) {
-            throw new IllegalArgumentException(DNI_CANT_CARACTERES_MIN);
+            throw new IllegalArgumentException(DNI_CANT_NUMEROS);
+        }
+        if (createPacienteDTO.dni().length() > 8) {
+            throw new IllegalArgumentException(DNI_CANT_NUMEROS);
         }
         if (createPacienteDTO.passwordPaciente() == null || createPacienteDTO.passwordPaciente().isBlank()) {
             throw new IllegalArgumentException(CONTRASENIA_VACIA_O_NULA);
@@ -61,7 +64,7 @@ public class Validator {
             throw new IllegalArgumentException(DNI_SOLO_ADMITE_NUMEROS);
         }
         if (usuario.getDni().length() < 8) {
-            throw new IllegalArgumentException(DNI_CANT_CARACTERES_MIN);
+            throw new IllegalArgumentException(DNI_CANT_NUMEROS);
         }
         if (usuario.getPassword() == null || usuario.getPassword().isBlank()) {
             throw new IllegalArgumentException(CONTRASENIA_VACIA_O_NULA);
