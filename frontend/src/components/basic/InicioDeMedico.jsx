13673da47282affe67a219be8543f068c399652c
amurua <ariel.murua@yahoo.com.ar>
2025-04-18 01:52:50 -0300

    Grilla de visualizar turnos del medico terminado

--- frontend/src/components/basic/InicioDeMedico.jsx
+++ frontend/src/components/basic/InicioDeMedico.jsx
@@ -4,6 +4,10 @@ import {useNavigate} from "react-router-dom";
 function InicioDeMedico() {
     const navigate = useNavigate();
 
+    const handleVerTurnos = () => {
+        navigate("/turnosDelMedico");
+    };
+
     return (
         <div className="d-flex flex-column justify-content-center align-items-center">
             <div className="d-flex flex-column justify-content-center align-items-center m-auto gap-4 text-center"
@@ -12,7 +16,7 @@ function InicioDeMedico() {
             }}>
                 <h1>¿Qué acción deseas realizar?</h1>
                 <Stack gap={2} className="m-auto w-25">
-                    <Button variant="outline-primary" size="lg" disabled>
+                    <Button variant="outline-primary" size="lg" onClick={handleVerTurnos}>
                         Ver turnos
                     </Button>
                 </Stack>
