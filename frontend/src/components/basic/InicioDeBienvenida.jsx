48aa90396ee0d3ef037aa95b7fec0494e7644f8c
amurua <ariel.murua@yahoo.com.ar>
2025-04-18 11:32:00 -0300

    Mensaje de bienvenida agregado

--- frontend/src/components/basic/InicioDeBienvenida.jsx
+++ frontend/src/components/basic/InicioDeBienvenida.jsx
@@ -7,7 +7,7 @@ function InicioDeBienvenida() {
     return (
         <div className="d-flex flex-column justify-content-center align-items-center text-center gap-4 m-auto" style={{height: '100vh', width: '400px'}}>
             <h1>
-                Bienvenidos a ConMed
+                    Bienvenidos a ConMed
             </h1>
             <div className="d-flex flex-column justify-content-center align-items-center gap-2" style={{width: '100%'}}>
                 <Button variant="outline-primary" size="lg" className="w-100" onClick={() => navigate("/login")}>
