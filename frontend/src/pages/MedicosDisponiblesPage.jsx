db94d32e4c28c70bbe692e78fcc8ec8a5a7b72bd
Wolf, Martin Leonardo <wolfmartinleonardo@gmail.com>
2025-05-18 19:35:45 -0300

    fix nombre y apellido

--- frontend/src/pages/MedicosDisponiblesPage.jsx
+++ frontend/src/pages/MedicosDisponiblesPage.jsx
@@ -74,10 +74,13 @@ const MedicosDisponiblesPage = () => {
         const coincideEspecialidad =
             especialidadSeleccionada === "Todas" || medico.especialidad === especialidadSeleccionada;
 
-        const coincideNombre =
-            nombreSeleccionado.trim() === "" ||
-            medico.nombre.toLowerCase().includes(nombreSeleccionado.toLowerCase()) ||
-            medico.apellido.toLowerCase().includes(nombreSeleccionado.toLowerCase());
+        // const coincideNombre =
+        //     nombreSeleccionado.trim() === "" ||
+        //     medico.nombre.toLowerCase().includes(nombreSeleccionado.toLowerCase()) ||
+        //     medico.apellido.toLowerCase().includes(nombreSeleccionado.toLowerCase());
+
+        const coincideNombre = nombreSeleccionado.trim() === "" ||
+            `${medico.nombre} ${medico.apellido}`.toLowerCase().includes(nombreSeleccionado.toLowerCase());
 
         const coincideDisponibilidad = mostrarSoloDisponibles ? medico.estaDisponible : true;
 
